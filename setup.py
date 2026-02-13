959136bcf18c96f7c9173368397ae0b86b27d916
Jepebu <jepebu@jepebu.com>
2026-02-13 16:30:29 -0500

    Modified base class name from PyShell to Shell to avoid confusion with the package pyshell.

--- setup.py
+++ setup.py
@@ -9,10 +9,6 @@ if not hasattr(configparser, 'SafeConfigParser'):
 from setuptools import setup, find_packages, Extension
 import sys
 
-extra_compile_args = []
-if sys.platform != "win32":
-    extra_compile_args = ["-fPIC", "-Wall"]
-
 shell_extension = Extension(
     'shellparser',                # The name of the module
     sources=['src/shellparser.c'] # The list of C files to compile
@@ -20,7 +16,7 @@ shell_extension = Extension(
 
 setup(
     name="shellhost",
-    version="1.0.2",
+    version="1.0.3",
     description="Turn Python functions into interactive shell commands in an isolated environment.",
     long_description="Provides an isolated interactive shell environment that you can import Python functions into as shell commands.",
     author="M. Bragg",
